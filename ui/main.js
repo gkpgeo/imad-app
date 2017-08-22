e709d3e67d36d1337348b79bdfa5d3a30eb2ee06
gkpgeo <30969876+gkpgeo@users.noreply.github.com>
2017-08-22 23:28:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,6 @@
 console.log('Loaded!');
 var element=document.getElementbyId('main-text');
-element.innerHtml='new value';
+element.innerHTML='new value';
 var img=document.getElementById('madi');
 img.onclick=function(){
     img.style.marginleft='100px';
